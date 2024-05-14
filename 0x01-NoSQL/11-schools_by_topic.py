e6a929eec3ed3739306d2a0b2daa642ccf958b59
ahmed-my <yahaya.mymohammed@gmail.com>
2024-05-14 09:22:14 +0000

    NoSQL

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,11 +6,11 @@
 def schools_by_topic(mongo_collection, topic):
     '''Returns the list of school having a specific topic.
     '''
-    topic_filter = {
+    specific_topic = {
         'topics': {
             '$elemMatch': {
                 '$eq': topic,
             },
         },
     }
-    return [doc for doc in mongo_collection.find(topic_filter)]
+    return [doc for doc in mongo_collection.find(specific_topic)]
