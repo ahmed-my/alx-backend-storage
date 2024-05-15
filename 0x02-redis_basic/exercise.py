a5bee9306ece40eeb183c3d457739ecac3ff5548
ahmed-my <yahaya.mymohammed@gmail.com>
2024-05-15 08:58:06 +0000

    Redis basic

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+''' A python script
+'''
+import redis
+import uuid
+
+
+class Cache:
+    def __init__(self):
+        """Initialize the Cache with a Redis client and flush the database."""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store the given data in Redis with a random key and return the key.
+
+        Args:
+            data: The data to be stored in Redis.
+
+        Returns:
+            str: The key under which the data is stored.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
